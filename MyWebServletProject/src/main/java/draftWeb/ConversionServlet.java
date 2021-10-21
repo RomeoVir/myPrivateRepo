4860a7a13f41c2266333dbc7eec37a35aca5fc99
RomeoVir <romeovirgilio@gmail.com>
2021-10-22 00:05:45 +0200

    Sleep time :)

---
+++ MyWebServletProject/src/main/java/draftWeb/ConversionServlet.java
@@ -0,0 +1,45 @@
+package draftWeb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/ConversionServlet")
+public class ConversionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String tipoSorgente = (request.getParameter("daGradi"));
+		String tipoDestinazione = (request.getParameter("aGradi"));		
+		String temperatura = (request.getParameter("valore"));
+		double tempDouble = Double.parseDouble(temperatura);
+		double risultato = 0;
+		
+		if(tipoSorgente.equals("celsius")){
+			if (tipoDestinazione.equals("kelvin")){
+				risultato = tempDouble * 2;
+				
+			}
+		}
+		System.out.println("La temperatura è di " + risultato);
+		PrintWriter pw = response.getWriter();
+		response.setContentType("text/html");
+		pw.println("La temperatura è di " + risultato);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ MyWebServletProject/src/main/java/draftWeb/HelloServlet.java
@@ -0,0 +1,35 @@
+package draftWeb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+		urlPatterns = { "/HelloServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "FileName", value = "pippo.txt"), 
+				@WebInitParam(name = "Username", value = "ciccio")
+		})
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public HelloServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Benvenuto chiamata get: ").append(request.getContextPath());
+	}
+
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+//	}
+
+}
